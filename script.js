b9a157227d9530e810e1878c570216380ebf1fce
Gustavo Pucienik <gomespucienik@gmail.com>
2022-12-26 22:11:11 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const mudaTemaBtn = document.querySelector("#muda-tema")
 
-mudaTemaBtn.addEventListener("change", function(){
-
+function mudatema(){
     document.body.classList.toggle("dark")
+}
+
+loadtheme()
+
+function loadtheme(){
+    const darkmode = localStorage.getItem("dark")
+
+    if(darkmode)[
+        mudatema()
+    ]
+}
+
+
+mudaTemaBtn.addEventListener("change", function(){
+    mudatema()
+
+    localStorage.removeItem("dark")
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark", 1)
+    }
 })
